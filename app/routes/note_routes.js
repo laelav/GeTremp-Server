764176020249077e27dc221218af0f0444c9573d
Lael <laelav@gmail.com>
2018-12-26 21:31:04 +0200

    register.

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -15,12 +15,13 @@ module.exports = function (app, db) {
         */
 
     //CREATE
-    //Postman: POST:localhost:8000/notes
+    //Postman: POST:localhost:8000/register
     //Insert data into the DB.
-    app.post('/notes', (req, res) => {
-        const note = { text: req.body.body, title: req.body.title };
-
-        db.collection('MyCollection').insert(note, (err, result) => {
+    app.post('/register', (req, res) => {
+        const note = { id: req.body.id, phoneNumber: req.body.phoneNumber,firstName: req.body.firstName,lastName: req.body.lastName,email: req.body.email,address: req.body.address,image: req.body.image};
+        //const note = { text: req.body.body, title: req.body.title };
+        console.log("Register in bound " + req.toString());
+        db.collection('Clients').insert(note, (err, result) => {
             if (err) {
                 res.send({ 'error': 'An error has occurred in Post.' });
             } else {
