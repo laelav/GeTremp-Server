5f6d727a348e991c0013f71c3d738e7dbd2fe7c0
Lael <laelav@gmail.com>
2018-12-29 12:30:26 +0200

    getallroutinedrives

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,36 +1,109 @@
 module.exports = function (app, db) {
     var ObjectID = require('mongodb').ObjectID;
 
-    /*
-        const insertDocuments = (note, callback) => {
-            const collection = db.collection('MyCollection');
-            collection.insert(
-              [note],
-              (error, result) => {
-                  if (error) return process.exit(1);
-                  callback(result);
-              }
-            );
-        };
-        */
-
     //CREATE
     //Postman: POST:localhost:8000/register
     //Insert data into the DB.
     app.post('/register', (req, res) => {
         const note = { id: req.body.id, phoneNumber: req.body.phoneNumber,firstName: req.body.firstName,lastName: req.body.lastName,email: req.body.email,address: req.body.address,image: req.body.image};
         //const note = { text: req.body.body, title: req.body.title };
-        console.log(req.body.id);
-        console.log("Register in bound " + req.toString());
+        //console.log(req.body.id);
+        console.log("Register in progress " +req.body.id);
         db.collection('Clients').insert(note, (err, result) => {
             if (err) {
-                res.send({ 'error': 'An error has occurred in Post.' });
+                res.send({ 'error': 'An error has occurred in Post /register.' });
             } else {
                 //Send back to front.
                 res.send(result.ops[0]);
             }
         });
     });
+
+    app.post('/addgroup', (req, res) => {
+        const note = { name: req.body.name, image: req.body.image};
+        console.log("Register in progress " + req.body.name);
+        db.createCollection(req.body.name + "-Routine");
+        db.createCollection(req.body.name + "-Temp");
+        db.collection('AppGroups').insert(note, (err, result) => {
+            if (err) {
+                res.send({ 'error': 'An error has occurred in Post /addgroup.' });
+            } else {
+                //Send back to front.
+                console.log(result.ops[0]._id);
+                res.send(result.ops[0]);
+            }
+        });
+    });
+
+    app.post('/addroutinedrive', (req, res) => {
+        const note = {userid: req.body.userid, name: req.body.name, begincity: req.body.begincity, endcity: req.body.endcity, day: req.body.day, time: req.body.time, openslots: req.body.openslots, totalslots: req.body.totalslots};
+        console.log("Add Routine Drive in progress " + req.body.group + ": " +req.body.name + ", " +  req.body.begincity + " -> " + req.body.endcity);
+
+        db.collection(req.body.group + "-Routine").insert(note, (err, result) => {
+            if (err) {
+                res.send({ 'error': 'An error has occurred in Post /addroutinedrive.' });
+            } else {
+                //Send back to front.
+                console.log(result.ops[0]._id);
+                res.send(result.ops[0]);
+            }
+        });
+    });
+
+
+
+    app.get('/getallgroups', (req, res) => {
+        console.log("Get All Groups in progress");
+
+        db.collection('AppGroups').find({}).toArray(function(err, result) {
+            if (err) {
+                res.send({ 'error': 'An error has occurred in Get /getallgroups.' });
+            } else {
+                //Send back to front.
+                console.log(result);
+                res.send(result);
+            }
+        });
+    });
+
+
+    app.get('/getallroutinedrives/:group', (req, res) => {
+        const group = req.params.group;
+
+        console.log("Get All Routine Drives in progress " + group);
+
+        db.collection(group + '-Routine').find({}).toArray(function(err, result) {
+            if (err) {
+                res.send({ 'error': 'An error has occurred in Get /getallroutinedrives.' });
+            } else {
+                //Send back to front.
+                console.log(result);
+                res.send(result);
+            }
+        });
+    });
+
+    //-----------------------------------------------------------------------------------------------------------------------------------------------------
+/* HTTP Methods examples:
+
+
+    //CREATE
+    //Postman: POST:localhost:8000/register
+    //Insert data into the DB.
+    app.post('/register', (req, res) => {
+        const note = { text: req.body.body, title: req.body.title };
+        //console.log(req.body.id);
+        console.log("Register in progress " +req.body.id);
+        db.collection('Clients').insert(note, (err, result) => {
+            if (err) {
+                res.send({ 'error': 'An error has occurred in Post /register.' });
+            } else {
+                //Send back to front.
+                res.send(result.ops[0]);
+            }
+        });
+    });
+
 
     //READ
     //Postman: GET:localhost:8000/notes/5c0a57c4ceb48c3f804d58f9
@@ -95,6 +168,6 @@ module.exports = function (app, db) {
     });
 
 
-
+*/
 
 };
