9cdf8ce7680d4915649b0c5a0ce9ad37b119ea00
Lael <laelav@gmail.com>
2019-05-02 20:45:21 +0300

    /addtempdrive /addroutinedrive /getalltempdrives/:id/:group /deleteroutinedrive/:group/:userid/:driveid /deletetempdrive/:group/:userid/:driveid

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -63,29 +63,107 @@ module.exports = function (app, db) {
         //console.log(check);
         db.collection('Clients').findOne(check, function (err, result) {
             if (err) throw err;
+            //console.log("this is the result: " + result);
+            let thegroups = result.groups;
+
+            var hasGroupFlag = 0;
+            var GroupExistFlag = 0;
+            for (var i = 0; i < thegroups.length; i++) {
+                if (thegroups[i] == req.body.group.toLowerCase())
+                    hasGroupFlag = 1;
+            }
+
+
+            db.collection('AppGroups').find({}).toArray(function (err, result) {
+                if (err) {
+                    res.send({'error': 'An error has occurred in Get /getallgroups.'});
+                } else {
+                    //Send back to front.
+                    for (var i = 0; i < result.length; i++) {
+                        if (result[i] == req.body.group.toLowerCase())
+                            GroupExistFlag = 1;
+                    }
+
+                    if (hasGroupFlag == 1 && GroupExistFlag == 1) {
+                        db.collection(req.body.group.toLowerCase() + "-Routine").insert(note, (err, result) => {
+                            if (err) {
+                                res.send({'error': 'An error has occurred in Post /addroutinedrive.'});
+                            } else {
+                                //Send back to front.
+                                console.log(result.ops[0].id + " The drive has been added successfully!");
+                                res.send(result.ops[0].id + " The drive has been added successfully!");
+                            }
+                        });
+                    } else {
+                        console.log("The user id:" + req.body.userid + " name: " + req.body.name + " is not a part of the group '" + req.body.group + "'! or the group doesn't exists");
+                        res.send("The user id:" + req.body.userid + " name: " + req.body.name + " is not a part of the group '" + req.body.group + "'! or the group doesn't exists");
+                    }
+
+                }
+            });
+
+        });
+    });
+
+    app.post('/addtempdrive', (req, res) => {
+        const note = {
+            userid: req.body.userid,
+            name: req.body.name,
+            begincity: req.body.begincity,
+            endcity: req.body.endcity,
+            day: req.body.day,
+            time: req.body.time,
+            openslots: req.body.openslots,
+            totalslots: req.body.totalslots
+        };
+        console.log("Add Temp Drive in progress " + req.body.group + ": " + req.body.name + ", " + req.body.begincity + " -> " + req.body.endcity + " by userid " + req.body.userid);
+
+
+        const check = {'id': req.body.userid};
+        //console.log(check);
+        db.collection('Clients').findOne(check, function (err, result) {
+            if (err) throw err;
             //console.log(result);
             let thegroups = result.groups;
 
-            var flag = 0;
+            var hasGroupFlag = 0;
+            var GroupExistFlag = 0;
             for (var i = 0; i < thegroups.length; i++) {
-                if (thegroups[i] == req.body.group)
-                    flag = 1;
+                if (thegroups[i] == req.body.group.toLowerCase())
+                    hasGroupFlag = 1;
             }
+            db.collection('AppGroups').find({}).toArray(function (err, result) {
+                if (err) {
+                    res.send({'error': 'An error has occurred in Get /getallgroups.'});
+                } else {
+                    //Send back to front.
+                    //console.log(result);
+                    for (var i = 0; i < result.length; i++) {
+                        if (result[i].name == req.body.group.toLowerCase()){
+                            console.log(result[i].name + " + " + req.body.group.toLowerCase())
+                            GroupExistFlag = 1;
+                        }
 
-            if (flag == 1) {
-                db.collection(req.body.group + "-Routine").insert(note, (err, result) => {
-                    if (err) {
-                        res.send({'error': 'An error has occurred in Post /addroutinedrive.'});
-                    } else {
-                        //Send back to front.
-                        console.log(result.ops[0].id + " The drive has been added successfully!");
-                        res.send(result.ops[0].id + " The drive has been added successfully!");
                     }
-                });
-            } else {
-                console.log("The user id:" + req.body.userid + " name: " + req.body.name + " is not a part of the group '" + req.body.group + "'!");
-                res.send("The user id:" + req.body.userid + " name: " + req.body.name + " is not a part of the group '" + req.body.group + "'!");
-            }
+                    if (hasGroupFlag == 1 && GroupExistFlag == 1) {
+                        db.collection(req.body.group.toLowerCase() + "-Temp").insert(note, (err, result) => {
+                            if (err) {
+                                res.send({'error': 'An error has occurred in Post /addtempdrive.'});
+                            } else {
+                                //Send back to front.
+                                console.log(result.ops[0].id + " The drive has been added successfully!");
+                                res.send(result.ops[0].id + " The drive has been added successfully!");
+                            }
+                        });
+                    } else {
+                        console.log("The user id:" + req.body.userid + " name: " + req.body.name + " is not a part of the group '" + req.body.group + "'! or the group doesn't exists");
+                        res.send("The user id:" + req.body.userid + " name: " + req.body.name + " is not a part of the group '" + req.body.group + "'! or the group doesn't exists");
+                    }
+
+                }
+            });
+            //console.log("hasgroup is: " +hasGroupFlag + " groupexistflag is: " + GroupExistFlag);
+
         });
     });
 
@@ -223,6 +301,84 @@ module.exports = function (app, db) {
         });
     });
 
+
+    app.delete('/deleteroutinedrive/:group/:userid/:driveid', (req, res) => {
+        //getting the id from the url.
+        const group = req.params.group.toLowerCase();
+        const userid = req.params.userid;
+        const driveid = req.params.driveid;
+        //making a Json with "ObjectID".
+        const details = { '_id': new ObjectID(driveid) };
+
+
+        db.collection(group + '-Routine').find({}).toArray(function (err, result) {
+            if (err) {
+                res.send({'error': 'An error has occurred in Get /getallroutinedrivesbyadmin in deleteroutinedrive.'});
+            } else {
+                //Send back to front.
+                var ifDriveExistFlag = 0;
+                //console.log(result);
+                for (var i=0;i<result.length;i++){
+                    if (driveid == result[i]._id){
+                        ifDriveExistFlag = 1;
+                        if (userid == result[i].userid){
+                            db.collection(group + '-Routine').remove(details, (err, item) => {
+                                if (err) {
+                                    res.send({ 'error': 'An error has occurred in deleteroutinedrive' });
+                                } else {
+                                    //Send back to front.
+                                    console.log('Drive ' + driveid + ' deleted!');
+                                    res.send('Drive ' + driveid + ' deleted!');
+                                }
+                            });
+                        }
+                        else{
+                            res.send("The drive " + driveid + " hasn't been opened by the user " + userid);
+                        }
+                    }
+                }
+            }
+        });
+    });
+
+    app.delete('/deletetempdrive/:group/:userid/:driveid', (req, res) => {
+        //getting the id from the url.
+        const group = req.params.group.toLowerCase();
+        const userid = req.params.userid;
+        const driveid = req.params.driveid;
+        //making a Json with "ObjectID".
+        const details = { '_id': new ObjectID(driveid) };
+
+
+        db.collection(group + '-Temp').find({}).toArray(function (err, result) {
+            if (err) {
+                res.send({'error': 'An error has occurred in Get /getalltempdrivesbyadmin in deletempdrive.'});
+            } else {
+                //Send back to front.
+                var ifDriveExistFlag = 0;
+                //console.log(result);
+                for (var i=0;i<result.length;i++){
+                    if (driveid == result[i]._id){
+                        ifDriveExistFlag = 1;
+                        if (userid == result[i].userid){
+                            db.collection(group + '-Temp').remove(details, (err, item) => {
+                                if (err) {
+                                    res.send({ 'error': 'An error has occurred in deletetempdrive' });
+                                } else {
+                                    //Send back to front.
+                                    console.log('Drive ' + driveid + ' deleted!');
+                                    res.send('Drive ' + driveid + ' deleted!');
+                                }
+                            });
+                        }
+                        else{
+                            res.send("The drive " + driveid + " hasn't been opened by the user " + userid);
+                        }
+                    }
+                }
+            }
+        });
+    });
     //-----------------------------------------------------------------------------------------------------------------------------------------------------
     /* HTTP Methods examples:
 
